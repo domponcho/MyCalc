f679d413f548cd39a5ae4dda8961c5771ef96373
domponcho <70451219+domponcho@users.noreply.github.com>
2020-08-31 12:08:35 -0400

    Initial load of MyCalc
    Adding folder App_Start

---
+++ App_Start/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using MyCalc.Views;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Unity;
+
+namespace MyCalc.App_Start
+{
+    [Obsolete]
+    public class Bootstrapper:UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            Container.RegisterInstances();
+        }
+    }
+}
---
+++ App_Start/Unity_Config.cs
@@ -0,0 +1,14 @@
+﻿using MyCalc.Core.Calculators;
+using Unity;
+
+namespace MyCalc.App_Start
+{
+    public static class Unity_Config
+    {
+        public static IUnityContainer RegisterInstances(this IUnityContainer container)
+        {
+            container.RegisterType<ICalculator, ExpressionCalculator>();
+            return container;
+        }
+    }
+}
